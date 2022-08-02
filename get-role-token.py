5c938e6affc3429d47802028c27fe9b854341e7d
Sourcery AI <>
2022-08-02 18:58:20 +0000

    'Refactored by Sourcery'

--- get-role-token.py
+++ get-role-token.py
@@ -21,18 +21,28 @@ token = sys.argv[2] if len(sys.argv) > 2 else sys.exit(usage)
 
 user = json.loads(run_shell(["aws", "iam", "get-user"]))
 match = re.compile("arn\:aws\:iam\:\:(\d+):.*").match(user["User"]["Arn"])
-account_number = match.group(1)
+account_number = match[1]
 
-token = json.loads(run_shell(["aws", "sts", "assume-role",
-                              "--role-arn", "arn:aws:iam::{}:role/{}".format(account_number, role),
-                              "--role-session-name", "cli.user-{}".format(user["User"]["UserName"]),
-                              "--serial-number", "arn:aws:iam::{}:mfa/{}".format(account_number, user["User"]["UserName"]),
-                              "--token-code", "{}".format(token)]))
+token = json.loads(
+    run_shell([
+        "aws",
+        "sts",
+        "assume-role",
+        "--role-arn",
+        f"arn:aws:iam::{account_number}:role/{role}",
+        "--role-session-name",
+        f'cli.user-{user["User"]["UserName"]}',
+        "--serial-number",
+        f'arn:aws:iam::{account_number}:mfa/{user["User"]["UserName"]}',
+        "--token-code",
+        f"{token}",
+    ]))
 
-print("export AWS_ACCESS_KEY_ID={}".format(token["Credentials"]["AccessKeyId"]))
-print("export AWS_SECRET_ACCESS_KEY={}".format(token["Credentials"]["SecretAccessKey"]))
-print("export AWS_SESSION_TOKEN={}".format(token["Credentials"]["SessionToken"]))
-print("export AWS_SECURITY_TOKEN={}".format(token["Credentials"]["SessionToken"]))
-print("export AWS_ROLE_EXPIRATION={}".format(token["Credentials"]["Expiration"]))
-print("export AWS_ROLE_NAME={}".format(role))
+print(f'export AWS_ACCESS_KEY_ID={token["Credentials"]["AccessKeyId"]}')
+print(
+    f'export AWS_SECRET_ACCESS_KEY={token["Credentials"]["SecretAccessKey"]}')
+print(f'export AWS_SESSION_TOKEN={token["Credentials"]["SessionToken"]}')
+print(f'export AWS_SECURITY_TOKEN={token["Credentials"]["SessionToken"]}')
+print(f'export AWS_ROLE_EXPIRATION={token["Credentials"]["Expiration"]}')
+print(f"export AWS_ROLE_NAME={role}")
 
